9010ace836a4e72509cc6ea651694e498e7774ea
Satakun Utama <49134472+lines-of-codes@users.noreply.github.com>
2021-05-15 15:34:51 +0700

    moved Enums into another file

---
+++ python/langEnums.py
@@ -0,0 +1,20 @@
+from enum import Enum
+
+class Exceptions(Enum):
+	InvalidSyntax	= 100
+	AlreadyDefined	= 101
+	NotImplementedException	= 102
+	NotDefinedException		= 103
+	GeneralException		= 104
+	DivideByZeroException	= 105
+	InvalidValue	= 106
+
+class Types(Enum):
+	Boolean = 0
+	Integer = 1
+	Float	= 2
+	List	= 3
+	Dictionary = 4
+	Tuple	= 5
+	Dynamic	= 6
+	String	= 7
--- python/processor.py
+++ python/processor.py
@@ -1,24 +1,5 @@
-from enum import Enum
 from string import ascii_letters, digits
-
-class Exceptions(Enum):
-	InvalidSyntax	= 100
-	AlreadyDefined	= 101
-	NotImplementedException	= 102
-	NotDefinedException		= 103
-	GeneralException		= 104
-	DivideByZeroException	= 105
-	InvalidValue	= 106
-
-class Types(Enum):
-	Boolean = 0
-	Integer = 1
-	Float	= 2
-	List	= 3
-	Dictionary = 4
-	Tuple	= 5
-	Dynamic	= 6
-	String	= 7
+from langEnums import *
 
 # Trim & Split to make commands understandable by Lexer
 class CommandTrimmer:
